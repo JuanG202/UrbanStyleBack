2ab859948517f603d4e866c1a21075abe1645837
kevin <kevinr060799@gmail.com>
2026-03-09 16:46:29 -0500

    Se monta back para urbanStyle

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
-app.use(cors());
+// CORS configurado
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://urban-style-sigma.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 // carpeta de imágenes
@@ -16,7 +27,7 @@ if (process.env.UPLOAD_PATH) {
   app.use("/uploads", express.static(process.env.UPLOAD_PATH));
 }
 
-// conexión a MongoDB
+// conexión MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB conectado"))
 .catch(err => console.log(err));
@@ -26,11 +37,13 @@ app.use("/api/products", productRoutes);
 
 // solo levantar servidor en local
 if (require.main === module) {
+
   const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
   });
+
 }
 
 // exportar app para Vercel
